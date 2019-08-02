18f41a375811cc77706eae7b433f726f554c1fca
Esraakhaled2727 <53495727+Esraakhaled2727@users.noreply.github.com>
2019-08-02 11:15:11 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,30 @@
-// Select color input
-// Select size input
+let canvas=document.getElementBYId("pixelcanvas");
+let height=document.getElementBYId("inputHeight");
+let Width=document.getElementBYId("inputwidth");
+let sizepicker=document.getElementBYId("sizepicker");
+let color=document.getElementBYId("colorpicker");
 
-// When size is submitted by the user, call makeGrid()
+color.addEventlistener("click" , function(){});
 
+sizepicker.onsubmit=function(event){
+  event.preventDefault();
+   clearGrid();
+  makeGrid();
+};
 function makeGrid() {
-
-// Your code goes here!
-
+  for(let r=0; r<height.value; r++){
+    const row = canvas.insertRow(r);
+    for(let c=0; c<width.value; c++){
+      const cell = row.insertcell(c);
+      cell.addEventListener("click" , fillsquare);
+    }
+  }
+}
+function clearGrid(){
+  while(canvas.firstChild){
+    canvas.removeChild(canvas.firstChild);
+  } 
+}
+function fillsquare () {
+  this.setAttribute("style",`background-color: ${color.value}`);
 }
